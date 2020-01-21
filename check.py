793e9d9fc0dcd9c91084a5250e37b7a37945162f
Philip Dissert <philip.dissert@student.kit.edu>
2020-01-21 18:10:05 +0100

    Added Errorhandling, Multiple gitlab runners, and changed the Readme for better understanding

--- check.py
+++ check.py
@@ -4,20 +4,32 @@ import time
 import docker
 import re
 import json
-import pprint
 import subprocess
 import requests 
 import gitlab
+import configparser
 
-pp = pprint.PrettyPrinter(indent=4)
+class ConfigParserException(Exception):
+    pass
+
 client = docker.from_env()
 
-PRIVATE_TOKEN_GITLAB = 'HBkPMVNTR3EGqGyKJ3JJ'
-PRIVATE_TOKEN_GITHUB = '5e6816a03b3feb4e1a1dab668a79db351447cd70'
-PRIVATE_TOKEN_GITLABCOM = 'B6aqZ4C7Gv8tNUKQ1oBx'
+PRIVATE_TOKEN_GITLAB = ''
+PRIVATE_TOKEN_GITHUB = ''
+PRIVATE_TOKEN_GITLABCOM = ''
 
 LOCAL_GITLAB_URL = 'http://localhost:8000'
 SERVER_PORT = 9999
+
+try:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    PRIVATE_TOKEN_GITLAB = str(config['KEYS']['PRIVATE_TOKEN_GITLAB'])
+    PRIVATE_TOKEN_GITHUB = str(config['KEYS']['PRIVATE_TOKEN_GITHUB'])
+    PRIVATE_TOKEN_GITLABCOM = str(config['KEYS']['PRIVATE_TOKEN_GITLABCOM'])
+except:
+    print("Config config.ini could not get parsed")
+    exit()
 
 headerGitLab = {
         'PRIVATE-TOKEN': PRIVATE_TOKEN_GITLAB
@@ -32,54 +44,89 @@ def isVersionEqual(yourVersionString, otherVerionStringJsonList, isJson):
         otherVersionString = result.__next__().name    
     return ("v"+yourVersionString).__eq__(otherVersionString)
 
-def isVersionEqual2(yourVersionString, otherVerionStringJsonList): 
-    result = filter(lambda x: not "pre" in x["name"] and not "rc" in x["name"],otherVerionStringJsonList)   
-    otherVersionString = result.__next__()["name"]
-    return ("v"+yourVersionString).__eq__(otherVersionString)
+def isDockerLatest():
+    gitHubVersion = ''
+    try:
+        github = Github(PRIVATE_TOKEN_GITHUB)
+        gitHubVersion = github.get_repo("docker/docker-ce").get_releases()[0].title    
+    except:
+        print("Error while checking docker-repo. Do you have internet? Is your github.com key valid?")
+        raise
 
-def isDockerLatest():    
-    github = Github(PRIVATE_TOKEN_GITHUB)
-    gitHubVersion = github.get_repo("docker/docker-ce").get_releases()[0].title    
-    clientVersionWithTag = client.version()["Version"]
-    clientVersionRegex = re.search("[0-9\.]*",clientVersionWithTag)
-    clientVersion = clientVersionRegex.group(0)
+    clientVersion = ''
+    try:
+        clientVersionWithTag = client.version()["Version"]
+        clientVersionRegex = re.search("[0-9\.]*",clientVersionWithTag)
+        clientVersion = clientVersionRegex.group(0)
+    except:
+        print("Error while checking lokal docker, does it run? Does the libary work?")
+        raise
+
     if clientVersion.__eq__(gitHubVersion):
         return True
     else:
         return False
+    
 
 def isGitlabLatest():
     r = requests.get(LOCAL_GITLAB_URL+'/api/v4/version', headers=headerGitLab)
-    gitlabLocalVersionString = json.loads(r.text)["version"]
-
-    requestServer = json.loads(requests.get('https://api.github.com/repos/gitlabhq/gitlabhq/tags').text)    
+    gitlabLocalVersionString = ''
+    requestServer = ''
+    try:
+        gitlabLocalVersionString = json.loads(r.text)["version"]
+    except:
+        print("Local gitlab error does it run? Is the key valid?")
+        raise
+            
+    requestServer = json.loads(requests.get('https://api.github.com/repos/gitlabhq/gitlabhq/tags').text)
     return isVersionEqual(gitlabLocalVersionString, requestServer, isJson=True)
 
 def isGitlabRunnerLatest():
+    localVersions = []
+
     allRunners = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners', headers=headerGitLab).text)
-    gl = gitlab.Gitlab('http://gitlab.com', private_token=PRIVATE_TOKEN_GITLABCOM)
+    gl = gitlab.Gitlab('http://gitlab.com', private_token=PRIVATE_TOKEN_GITLABCOM)            
     for runnerFromAll in allRunners:
-        runner = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners/'+str(runnerFromAll["id"]), headers=headerGitLab).text)
-        return isVersionEqual(runner["version"], gl.projects.get(250833).releases.list(), isJson=False)
+        runner = {}
+        try:
+            runner = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners/'+str(runnerFromAll["id"]), headers=headerGitLab).text)
+        except:
+            print("Local gitlab error does it run? Is the key valid?")
+            raise
+        try:
+            localVersions.append(isVersionEqual(runner["version"], gl.projects.get(250833).releases.list(), isJson=False))
+        except:
+            print("GitLab.com error do you have internet? Is the key valid?")
+            raise
 
-#print(isGitlabLatest())
-#print(isDockerLatest())
-#print(isGitlabRunnerLatest())
-
-
+    
+    return localVersions
 
 if __name__ == '__main__':
+
     # Start up the server to expose the metrics.
     start_http_server(SERVER_PORT)
     # Generate some requests.
     while True:
-        #g = Gauge("toolcheck_is_gitlab_latest", "0 if gitlab needs to be updated else 1")
-        #g.set(isGitlabLatest())
+        try:
+            g = Gauge("toolcheck_is_gitlab_latest", "0 if gitlab needs to be updated else 1")
+            g.set(isGitlabLatest())
+        except:
+            print("toolcheck_is_gitlab_latest will not get shown")
 
-        #g = Gauge("toolcheck_is_docker_latest", "0 if docker needs to be updated else 1")
-        #g.set(isDockerLatest())
-
-        #g = Gauge("toolcheck_is_gitlab_runner_latest", "0 if gitlab-runner needs to be updated else 1")
-        #g.set(isGitlabRunnerLatest())
+        try:
+            g = Gauge("toolcheck_is_docker_latest", "0 if docker needs to be updated else 1")
+            g.set(isDockerLatest())
+        except:
+            print("toolcheck_is_docker_latest will not get shown")
+        
+        try:
+            runnercount = 0
+            for runnerBoolean in isGitlabRunnerLatest():
+                g = Gauge("toolcheck_is_gitlab_runner_latest_"+ str(runnercount), "0 if gitlab-runner needs to be updated else 1")
+                g.set(runnerBoolean)
+                runnercount = runnercount + 1
+        except:            
+            print("toolcheck_is_gitlabrunner_latest will not get shown")
 
         time.sleep(60*10)
