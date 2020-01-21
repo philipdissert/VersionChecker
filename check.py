3c7ebb0995f32836f82592c17432067f014edfaf
Fabian <fabian.roepke@campusjaeger.de>
2020-01-21 15:56:22 +0100

    data source naming

--- check.py
+++ check.py
@@ -12,9 +12,9 @@ import gitlab
 pp = pprint.PrettyPrinter(indent=4)
 client = docker.from_env()
 
-PRIVATE_TOKEN_GITLAB = ''
-PRIVATE_TOKEN_GITHUB = ''
-PRIVATE_TOKEN_GITLABCOM = ''
+PRIVATE_TOKEN_GITLAB = 'HBkPMVNTR3EGqGyKJ3JJ'
+PRIVATE_TOKEN_GITHUB = '5e6816a03b3feb4e1a1dab668a79db351447cd70'
+PRIVATE_TOKEN_GITLABCOM = 'B6aqZ4C7Gv8tNUKQ1oBx'
 
 LOCAL_GITLAB_URL = 'http://localhost:8000'
 SERVER_PORT = 9999
@@ -55,12 +55,12 @@ def isGitlabLatest():
     requestServer = json.loads(requests.get('https://api.github.com/repos/gitlabhq/gitlabhq/tags').text)    
     return isVersionEqual(gitlabLocalVersionString, requestServer, isJson=True)
 
-def isGitlabRunnerLatest():
-    allRunners = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners', headers=headerGitLab).text)
-    gl = gitlab.Gitlab('http://gitlab.com', private_token=PRIVATE_TOKEN_GITLABCOM)
-    for runnerFromAll in allRunners:
-        runner = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners/'+str(runnerFromAll["id"]), headers=headerGitLab).text)
-        return isVersionEqual(runner["version"], gl.projects.get(250833).releases.list(), isJson=False)
+#def isGitlabRunnerLatest():
+#    allRunners = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners', headers=headerGitLab).text)
+#    gl = gitlab.Gitlab('http://gitlab.com', private_token=PRIVATE_TOKEN_GITLABCOM)
+#    for runnerFromAll in allRunners:
+#        runner = json.loads(requests.get(LOCAL_GITLAB_URL+'/api/v4/runners/'+str(runnerFromAll["id"]), headers=headerGitLab).text)
+#        return isVersionEqual(runner["version"], gl.projects.get(250833).releases.list(), isJson=False)
 
 #print(isGitlabLatest())
 #print(isDockerLatest())
@@ -73,13 +73,13 @@ if __name__ == '__main__':
     start_http_server(SERVER_PORT)
     # Generate some requests.
     while True:
-        g = Gauge("is_gitlab_latest", "0 if gitlab needs to be updated else 1")
+        g = Gauge("toolcheck_is_gitlab_latest", "0 if gitlab needs to be updated else 1")
         g.set(isGitlabLatest())
 
-        g = Gauge("is_docker_latest", "0 if docker needs to be updated else 1")
+        g = Gauge("toolcheck_is_docker_latest", "0 if docker needs to be updated else 1")
         g.set(isDockerLatest())
 
-        g = Gauge("is_gitlab_runner_latest", "0 if gitlab-runner needs to be updated else 1")
-        g.set(isGitlabRunnerLatest())
+        #g = Gauge("toolcheck_is_gitlab_runner_latest", "0 if gitlab-runner needs to be updated else 1")
+        #g.set(isGitlabRunnerLatest())
 
         time.sleep(60*10)
